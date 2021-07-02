87f7d2e5d63d2f7875f0e0a25616a53f1db8e3ec
radluki <radluki@gmail.com>
2021-07-02 18:47:52 +0200

    update user, add user, admin tests

--- lapp/lapp/settings.py
+++ lapp/lapp/settings.py
@@ -121,3 +121,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'core.User'
+
+DEFAULT_AUTO_FIELD='django.db.models.AutoField'
--- test.ps1
+++ test.ps1
@@ -1,1 +1,15 @@
+function exitIfFail {
+    if (! $?)
+    {
+        echo "FAILURE"
+        exit
+    }
+}
+
+python manage.py test
+exitIfFail
+
+flake8
+exitIfFail
+
 docker-compose run --rm app sh -c "python manage.py test && flake8"
